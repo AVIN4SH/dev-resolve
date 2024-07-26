11b51f695fc1f5e74132f51e4fd1c21782aae096
Avinash <savinashkumarsingh619@gmail.com.com>
2024-07-26 21:19:12 +0530

    update colors in clerk auth popup

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,20 @@
 import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignIn />;
+  return (
+    <SignIn
+      appearance={{
+        elements: {
+          formButtonPrimary: {
+            fontSize: 14,
+            textTransform: "none",
+            backgroundColor: "#181818",
+            "&:hover, &:focus, &:active": {
+              backgroundColor: "#212121",
+            },
+          },
+        },
+      }}
+    />
+  );
 }
--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,20 @@
 import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignUp />;
+  return (
+    <SignUp
+      appearance={{
+        elements: {
+          formButtonPrimary: {
+            fontSize: 14,
+            textTransform: "none",
+            backgroundColor: "#181818",
+            "&:hover, &:focus, &:active": {
+              backgroundColor: "#212121",
+            },
+          },
+        },
+      }}
+    />
+  );
 }
